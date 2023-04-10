c65300592d32dd3125de9ab9a4e40609ec8b568b
escanoru <yitzoyi@outlook.com>
2023-04-10 05:17:46 +0000

    Adding joke api code

--- go-simple-api.go
+++ go-simple-api.go
@@ -9,10 +9,19 @@ import (
 
 type setTime struct {
 	CurrentTime string `json:"current_time"`
+	Joke        string `json:"current_joke"`
+}
+
+// Joke struct that defines the fields that will hold the json return data
+type Joke struct {
+	IconUrl string
+	Id      string
+	URL     string
+	Value   string
 }
 
 var (
-	port     = 8080
+	port     = 8081
 	endpoint = fmt.Sprintf("0.0.0.0:%d", port)
 )
 
@@ -28,9 +37,33 @@ func main() {
 
 // showCurrentTime returns the current time on the server
 func showCurrentTime(w http.ResponseWriter, r *http.Request) {
-	currentTime := []setTime{
+	holdData := &[]setTime{
 		{CurrentTime: http.TimeFormat},
+		{Joke: callJoker()},
 	}
 
-	json.NewEncoder(w).Encode(currentTime)
+	json.NewEncoder(w).Encode(*holdData)
+}
+
+// callJoker calls jokeFetcher()
+func callJoker() string {
+	jokeText, err := jokeFetcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// log.Printf("%+v\n\n", jokeText)
+	return jokeText
+}
+
+// jokeFetcher fetches a random joke and returns it along with an error.
+func jokeFetcher() (string, error) {
+	log.Println("Fetching Joke")
+	jokeInstance := &Joke{}
+	resp, err := http.Get("https://api.chucknorris.io/jokes/random")
+	if err != nil {
+		return "", err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(jokeInstance)
+	return jokeInstance.Value, err
 }
